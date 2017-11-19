359310d8d15db65d4fcab036b3f5199861f4d0a7
zfxiao <luckygirlalisa@gmail.com>
2017-11-19 23:16:56 +0800

    Move update quality methods into item.

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -17,77 +17,14 @@ class GildedRose {
         List<Item> updatedItems = new ArrayList<>();
         for (Item item : items) {
             Item updatedItem = item;
-            updatedItem = updateNormalItem(updatedItem);
-            updatedItem = updateQualityForBackStage(updatedItem);
-            updatedItem = updateQualityForAgedBrie(updatedItem);
+            updatedItem = updatedItem.updateNormalItem();
+            updatedItem = updatedItem.updateQualityForBackStage();
+            updatedItem = updatedItem.updateQualityForAgedBrie();
 
-            updatedItem = updateSellInForSulfuras(updatedItem);
+            updatedItem = updatedItem.updateSellInForSulfuras();
 
-            updatedItems.add(updateWhenNoSellin(updatedItem));
+            updatedItems.add(updatedItem.updateWhenNoSellin());
         }
     }
 
-    private Item updateWhenNoSellin(Item item) {
-        if (item.sellIn < 0) {
-            if (!item.name.equals(AGED_BRIE)) {
-                if (!item.name.equals(BACKSTAGE_PASSES)) {
-                    if (item.quality > 0) {
-                        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                            item.quality = item.quality - 1;
-                        }
-                    }
-                } else {
-                    item.quality = 0;
-                }
-            } else {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-                }
-            }
-        }
-
-        return item;
-    }
-
-    private Item updateSellInForSulfuras(Item item) {
-        if (!item.name.equals(SULFURAS_HAND)) {
-            item.sellIn = item.sellIn - 1;
-        }
-        return item;
-    }
-
-    private Item updateQualityForAgedBrie(Item item) {
-        if (item.quality < 50 && item.name.equals(AGED_BRIE) ) {
-            item.quality += 1;
-        }
-
-        return item;
-    }
-
-    private Item updateQualityForBackStage(Item item) {
-        if (item.quality < 50 && item.name.equals(BACKSTAGE_PASSES)) {
-            item.quality += 1;
-
-            if (item.quality < 50) {
-                int incrementWhenSellin11days = item.sellIn < 11 ? 1 : 0;
-                int incrementWhenSellin6days = item.sellIn < 6 ? 1 + incrementWhenSellin11days: incrementWhenSellin11days;
-                item.quality += incrementWhenSellin6days;
-            }
-        }
-
-        return item;
-    }
-
-    private Item updateNormalItem(Item item) {
-        if (!item.name.equals(AGED_BRIE)
-                && !item.name.equals(BACKSTAGE_PASSES)) {
-            if (item.quality > 0) {
-                if (!item.name.equals(SULFURAS_HAND)) {
-                    item.quality -= 1;
-                }
-            }
-        }
-
-        return item;
-    }
 }
--- src/main/java/com/gildedrose/Item.java
+++ src/main/java/com/gildedrose/Item.java
@@ -18,4 +18,67 @@ public class Item {
    public String toString() {
         return this.name + ", " + this.sellIn + ", " + this.quality;
     }
+
+    Item updateNormalItem() {
+        if (!name.equals(GildedRose.AGED_BRIE) && !name.equals(GildedRose.BACKSTAGE_PASSES)) {
+            if (quality > 0) {
+                if (!name.equals(GildedRose.SULFURAS_HAND)) {
+                    quality -= 1;
+                }
+            }
+        }
+
+        return this;
+    }
+
+    Item updateQualityForBackStage() {
+        if (quality < 50 && name.equals(GildedRose.BACKSTAGE_PASSES)) {
+            quality += 1;
+
+            if (quality < 50) {
+                int incrementWhenSellin11days = sellIn < 11 ? 1 : 0;
+                int incrementWhenSellin6days = sellIn < 6 ? 1 + incrementWhenSellin11days: incrementWhenSellin11days;
+                quality += incrementWhenSellin6days;
+            }
+        }
+
+        return this;
+    }
+
+    Item updateQualityForAgedBrie() {
+        if (quality < 50 && name.equals(GildedRose.AGED_BRIE) ) {
+            quality += 1;
+        }
+
+        return this;
+    }
+
+    Item updateSellInForSulfuras() {
+        if (!name.equals(GildedRose.SULFURAS_HAND)) {
+            sellIn = sellIn - 1;
+        }
+        return this;
+    }
+
+    Item updateWhenNoSellin() {
+        if (sellIn < 0) {
+            if (!name.equals(GildedRose.AGED_BRIE)) {
+                if (!name.equals(GildedRose.BACKSTAGE_PASSES)) {
+                    if (quality > 0) {
+                        if (!name.equals("Sulfuras, Hand of Ragnaros")) {
+                            quality = quality - 1;
+                        }
+                    }
+                } else {
+                    quality = 0;
+                }
+            } else {
+                if (quality < 50) {
+                    quality = quality + 1;
+                }
+            }
+        }
+
+        return this;
+    }
 }
